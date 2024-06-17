bb13a1f52c4879b8b74d8f3847de3038debfec29
Sean Collins <sean@cllns.com>
2024-06-17 14:14:02 -0600

    Support module around class

--- lib/rooby/class.rb
+++ lib/rooby/class.rb
@@ -4,33 +4,68 @@
 
 module Rooby
   class Class
-    def initialize(name, parent = nil)
+    def initialize(name, parent = nil, modules: [])
       @name = name
       @parent = parent
+      @modules = modules
     end
 
     def to_s
-      if parent
-        with_parent
+      if modules.any?
+        with_modules
       else
-        without_parent
+        without_modules
       end
     end
 
     private
 
-    attr_reader :name, :parent
+    attr_reader :name, :parent, :modules
 
-    def with_parent
+    def without_modules
+      if parent
+        with_parent_and_without_modules
+      else
+        without_parent_and_without_modules
+      end
+    end
+
+    def with_parent_and_without_modules
       <<~OUTPUT
         class #{@name} < #{@parent}
         end
       OUTPUT
     end
 
-    def without_parent
+    def without_parent_and_without_modules
       <<~OUTPUT
         class #{@name}
+        end
+      OUTPUT
+    end
+
+    def with_modules
+      if parent
+        with_parent_and_with_modules
+      else
+        without_parent_and_with_modules
+      end
+    end
+
+    def with_parent_and_with_modules
+      <<~OUTPUT
+        module #{@modules.join('::')}
+          class #{@name} < #{@parent}
+          end
+        end
+      OUTPUT
+    end
+
+    def without_parent_and_with_modules
+      <<~OUTPUT
+        module #{@modules.join('::')}
+          class #{@name}
+          end
         end
       OUTPUT
     end
--- spec/rooby/class_spec.rb
+++ spec/rooby/class_spec.rb
@@ -3,20 +3,52 @@
 RSpec.describe Rooby::Class do
   describe "top-level" do
     it "generates class without parent class" do
-      expect(Rooby::Class.new("Greeter").to_s).to eq(
-        <<~OUTPUT
-          class Greeter
-          end
-        OUTPUT
+      expect(Rooby::Class.new("Greeter").to_s).to(
+        eq(
+          <<~OUTPUT
+            class Greeter
+            end
+          OUTPUT
+        )
       )
     end
 
     it "generates class with parent class" do
-      expect(Rooby::Class.new("Greeter", "BaseService").to_s).to eq(
-        <<~OUTPUT
-          class Greeter < BaseService
-          end
-        OUTPUT
+      expect(Rooby::Class.new("Greeter", "BaseService").to_s).to(
+        eq(
+          <<~OUTPUT
+            class Greeter < BaseService
+            end
+          OUTPUT
+        )
+      )
+    end
+  end
+
+  describe "with module" do
+    it "generates class without parent class" do
+      expect(Rooby::Class.new("Greeter", modules: %w[Services]).to_s).to(
+        eq(
+          <<~OUTPUT
+            module Services
+              class Greeter
+              end
+            end
+          OUTPUT
+        )
+      )
+    end
+
+    it "generates class with parent class" do
+      expect(Rooby::Class.new("Greeter", "BaseService", modules: %w[Services]).to_s).to(
+        eq(
+          <<~OUTPUT
+            module Services
+              class Greeter < BaseService
+              end
+            end
+          OUTPUT
+        )
       )
     end
   end
