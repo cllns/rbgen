95af9c81328d9d1b919738a2399e5e5d8083fcba
Sean Collins <sean@cllns.com>
2024-06-17 13:49:56 -0600

    Refactor

--- lib/rooby/class.rb
+++ lib/rooby/class.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # frozen_string_literal
 
 module Rooby
@@ -9,20 +11,28 @@ module Rooby
 
     def to_s
       if parent
-        <<~OUTPUT
-          class #{@name} < #{@parent}
-          end
-        OUTPUT
+        with_parent
       else
-        <<~OUTPUT
-          class #{@name}
-          end
-        OUTPUT
+        without_parent
       end
     end
 
     private
 
     attr_reader :name, :parent
+
+    def with_parent
+      <<~OUTPUT
+        class #{@name} < #{@parent}
+        end
+      OUTPUT
+    end
+
+    def without_parent
+      <<~OUTPUT
+        class #{@name}
+        end
+      OUTPUT
+    end
   end
 end
--- spec/rooby/class_spec.rb
+++ spec/rooby/class_spec.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe Rooby::Class do
-  it "generates basic class with no parent class" do
-    expect(Rooby::Class.new("Greeter").to_s).to eq(
-      <<~OUTPUT
-        class Greeter
-        end
-      OUTPUT
-    )
-  end
+  describe "top-level" do
+    it "generates class without parent class" do
+      expect(Rooby::Class.new("Greeter").to_s).to eq(
+        <<~OUTPUT
+          class Greeter
+          end
+        OUTPUT
+      )
+    end
 
-  it "generates class with parent class" do
-    expect(Rooby::Class.new("Greeter", "BaseService").to_s).to eq(
-      <<~OUTPUT
-        class Greeter < BaseService
-        end
-      OUTPUT
-    )
+    it "generates class with parent class" do
+      expect(Rooby::Class.new("Greeter", "BaseService").to_s).to eq(
+        <<~OUTPUT
+          class Greeter < BaseService
+          end
+        OUTPUT
+      )
+    end
   end
 end
